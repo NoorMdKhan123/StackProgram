a4e93d44c6257dd6f32654fea23b71f19c049091
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-07 17:08:16 +0530

    Added in Stack using push method

---
+++ Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAssignment
+{
+    class Node
+    {
+      public int data;
+       public Node next;
+        public Node(int i)
+        {
+            data = i;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StackAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+
+
+        }
+    }
+}
